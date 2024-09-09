4f3477ed586e3a6c0958c9da1495477625cf4d48
nuno <nunoserol95@gmail.com>
2024-09-10 00:22:46 +0100

    User input changes

--- sync.py
+++ sync.py
@@ -3,7 +3,9 @@ import logging
 
 def main():
     teste()
-    print(getArgs()[1])
+    #print(getArgs()[1])
+    p1T=UserInput()
+    print(p1T.folderPathSource, p1T.folderPathReplica, p1T.synchronizationInterval, p1T.logFilePath)
     createLogFile()
     createReplica()
     while True:
@@ -20,16 +22,21 @@ def createLogFile():
 def teste():
     print("hello world test")
 
-def getArgs():
-    folderPathSource=sys.argv[1]
-    folderPathReplica=sys.argv[2]
-    synchronizationInterval=sys.argv[3]
-    logFilePath=sys.argv[4]
-    #print("folderPathSource", folderPathSource)
-    #print("folderPathReplica", folderPathReplica)
-    #print("synchronizationInterval", synchronizationInterval)
-    #print("logFilePath", logFilePath)
-    return folderPathSource, folderPathSource, synchronizationInterval, logFilePath
+
+#Class used to store all the user input
+#1)folderPathSource
+#2)folderPathReplica
+#3)synchronizationInterval
+#4)logFilePath
+class UserInput:
+    def __init__(self):
+        self.folderPathSource = sys.argv[1]
+        self.folderPathReplica = sys.argv[2]
+        self.synchronizationInterval=sys.argv[3]
+        self.logFilePath=sys.argv[4]
+
+#def getArgs():
+    #return folderPathSource, folderPathSource, synchronizationInterval, logFilePath
 
 
 main()
